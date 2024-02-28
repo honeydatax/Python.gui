f2fdc2e5fd4bbb3637a753a5803c4545aa942976
lisbon <16432726+honeydatax@users.noreply.github.com>
2024-02-28 21:52:29 +0000

    Add files via upload

--- optbox.py
+++ optbox.py
@@ -3,15 +3,24 @@ import tkinter as tk
 Window=None
 lists1=[]
 lists2=[]
+lists3=[]
+
 canvas=None
 def drawbox(canvas,list2,l1,n):
-    canvas.create_oval((list2[n][0], list2[n][1]), (list2[n][2], list2[n][3]), fill="white")
+    colors="white"
     if l1:
-        canvas.create_oval((list2[n][0]+5, lists2[n][1]+5), (list2[n][2]-5, list2[n][3]-5), fill="Black")
+        colors="Black"
+
+    canvas.create_oval((list2[n][0], list2[n][1]), (list2[n][2], list2[n][3]), fill="white")
+    
+    r1=canvas.create_oval((list2[n][0]+5, lists2[n][1]+5), (list2[n][2]-5, list2[n][3]-5) ,outline="white", fill=colors)
+    return r1
 def msgbox(msgs:str,color:str):
     global Window
     global lists1
     global lists2
+    global lists3
+
     global canvas
     Window = tk.Toplevel(bg=color)
     canvas = tk.Canvas(Window, width=800, height=600, bg=color)
@@ -20,12 +29,12 @@ def msgbox(msgs:str,color:str):
     canvas.pack()
     iii:int=0
     for n in range(10):
-        lists2=lists2+[[10,n*25+10,10+20,n*25+10+20,"black"]]
+        lists2=lists2+[[10,n*25+10,10+20,n*25+10+20]]
         if iii==0:
             lists1=lists1+[True]
         else:
             lists1=lists1+[False]
-        drawbox(canvas,lists2,lists1[n],n)
+        lists3=lists3+[drawbox(canvas,lists2,lists1[n],n)]
         iii+=1
 
 def handle_key(event):
@@ -37,6 +46,8 @@ def handle_mouse_click(event):
     global Window
     global lists1
     global lists2
+    global lists3
+
     global canvas
     x=event.x
     y=event.y
@@ -48,7 +59,10 @@ def handle_mouse_click(event):
                 lists1[nn]=False
                 if nn==n:
                     lists1[nn]=True
-                drawbox(canvas,lists2,lists1[nn],nn)
+                colors="white"
+                if lists1[nn]:
+                    colors="black"
+                canvas.itemconfig(lists3[nn],fill=colors)
             nn=n
             
     if nn<0:
